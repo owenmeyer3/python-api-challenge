47cd6d4d9cd058c0b7908ee3c267e2633393ec77
owenmeyer3 <owenmeyer3@gmail.com>
2020-10-21 12:43:46 -0500

    modified vacationPy to create humidity heatmap

--- common/api_keys.py
+++ common/api_keys.py
@@ -2,16 +2,4 @@
 weather_api_key = "d5499d75f9c25485c66aea61adf8f07c"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
-
-
-#OpenWeatherAPI
-'''
-https://openweathermap.org/current
-
-api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
-
-api.openweathermap.org/data/2.5/weather?q={city name},{state code}&appid={API key}
-
-api.openweathermap.org/data/2.5/weather?q={city name},{state code},{country code}&appid={API key}
-'''
+g_key = "AIzaSyDDBhXcb1hTN6nWybZ5WvJk_yUiISAlSAA"
